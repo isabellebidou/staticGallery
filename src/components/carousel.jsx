5c5d03b672d42981b498f3ffa381358736890449
isabelle bidou <isa.bidou@gmail.com>
2026-03-31 10:21:02 +0200

    implemented lazy loading and moved pics to AWS s3

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,12 +1,13 @@
 export default function Carousel({ photo, onClose, next, back, carousel }) {
-  console.log("carousel   - visible: ", carousel)
+  const BASE_URL = "https://ib-photo-gallery.s3.amazonaws.com/"
   if (!carousel) return null
   return (
     <>
       <div className="carousel-overlay">
         <img
           className="displayimage"
-          src={"http://isabellebidou.com/images/" + photo.photoName}
+          //src={"http://isabellebidou.com/images/" + photo.photoName}
+          src={BASE_URL + photo.photoName}
           alt="{photo.photoName}"
         />
 
--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,24 +1,24 @@
 export default function Gallery({ data, onSelectPhoto }) {
+  const BASE_URL = "https://ib-photo-gallery.s3.amazonaws.com/"
   return (
     <>
       <div className="maincontent">
         <div className="grid-container">
-          {data.map((photo, i) => (
-            <div className="tooltip">
-              <div className={`photothumbnail`} key={i}>
+          {data.map((photo) => (
+            <div className="tooltip" key={photo.photoId}>
+              <div className="photothumbnail">
                 <img
                   onClick={() => onSelectPhoto(photo)}
-                  src={
-                    "http://isabellebidou.com/images/" + photo.photoThumbnail
-                  }
+                  //src={`http://isabellebidou.com/images/${photo.photoThumbnail}`}
+                  src={`${BASE_URL}${photo.photoThumbnail}`}
                   alt={photo.photoThumbnail}
+                  loading="lazy"
+                  width="200"
                 />
+
                 <span
                   className="tooltiptext"
                   onClick={() => onSelectPhoto(photo)}
-                  src={
-                    "http://isabellebidou.com/images/" + photo.photoThumbnail
-                  }
                 >
                   {[photo.photoPlace, photo.photoCountry, photo.photoComments]
                     .filter((t) => t && t.trim() !== "")
